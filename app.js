71e2a17647c63bababc7d675849b7f8566cf1b7c
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-05-18 15:07:40 +0545

    fileExtensionLimiter middleware

--- app.js
+++ app.js
@@ -4,16 +4,27 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+const fileExtensionLimiter = require("./middleware/fileExtensionLimiter");
+const fileSizeLimiter = require("./middleware/fileSizeLimiter");
+const filesPayloadExists = require("./middleware/filesPayloadExists");
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/upload", fileUpload({ createParentPath: true }), (req, res) => {
-  const files = req.files;
-  console.log(files);
+app.post(
+  "/upload",
+  fileUpload({ createParentPath: true }),
+  filesPayloadExists,
+  fileExtensionLimiter([".png", ".jpeg", ".jpg"]),
+  fileSizeLimiter,
+  (req, res) => {
+    const files = req.files;
+    console.log(files);
 
-  return res.json({ status: "logged", message: "logged" });
-});
+    return res.json({ status: "logged", message: "logged" });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
